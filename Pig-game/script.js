f3a97c8e18850a03d55682c16e91b5b78d615caa
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-03-06 01:04:41 +0900

    pig game 完了

--- Pig-game/script.js
+++ Pig-game/script.js
@@ -15,36 +15,83 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //最初の状態
-score0EL.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  diceEl.classList.add('hidden');
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0EL.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player-active');
+};
+init();
 
 //サイコロを回す
 btnRoll.addEventListener('click', function () {
-  //1.ランダムなサイコロの目を作る
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //2. サイコロの目を表示する。
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    //2. サイコロの目を表示する。
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  //3. もしサイコロの目は１になったらプレイヤーをチェンジチェンジ
-  if (dice !== 1) {
-    //現在の得点に追加する。
-    currentScore = currentScore + dice;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-  } else {
-    //プレイヤーをチェンジ
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    //3. もしサイコロの目は１になったらプレイヤーをチェンジチェンジ
+    if (dice !== 1) {
+      //現在の得点に追加する。
+      currentScore = currentScore + dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      //プレイヤーをチェンジ
+      switchPlayer();
+    }
   }
+  //1.ランダムなサイコロの目を作る
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //2. プレーヤーのスコア>= 100かをチェックします。
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+  //現在のスコアをアクティブなプレーヤのスコアに追加します。
+});
+// ゲームを終了する。
+//プレーヤーをチェンジ
+
+btnNew.addEventListener('click', function () {
+  init();
 });
