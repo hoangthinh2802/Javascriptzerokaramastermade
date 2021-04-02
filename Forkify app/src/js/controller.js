e249f0a0d54de3bcd7c74dda81accdc5af637df7
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-04-02 17:04:19 +0900

    Forkify App Refactoring for MVC , helpers and configuration files

---
+++ Forkify app/src/js/controller.js
@@ -0,0 +1,43 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// import recipeView from './views/recipeView.js';
+
+// console.log(icons);
+
+const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1,  loading recipe
+    await model.loadRecipe(id);
+
+    // 2, Rendering recipe
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
